26f265a437970959b0985ccf7983e69d321d1ca5
MJaroslav <rcr3211@gmail.com>
2019-05-02 23:47:50 +0300

    1.6.0 hotfix (version name)

--- src/main/java/mjaroslav/mcmods/mjutils/mod/lib/ModInfo.java
+++ src/main/java/mjaroslav/mcmods/mjutils/mod/lib/ModInfo.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 public class ModInfo {
     public static final String MODID = "mjutils";
     public static final String NAME = "MJUtils";
-    public static final String VERSION = "1.5.0";
+    public static final String VERSION = "1.6.0";
     public static final String GUI_FACTORY = "mjaroslav.mcmods.mjutils.mod.client.gui.GUIFactory";
 
     public static Logger LOG = LogManager.getLogger(NAME);
