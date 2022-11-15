cfbb4aa2d874df7e0af4f41cfc6b28c272a424b6
Yaroslav Novitsky <rcr3211@gmail.com>
2022-11-15 16:09:32 +0300

    add: obj and gl list models from https://github.com/MJaroslav/Heracleum;

---
+++ src/main/java/io/github/mjaroslav/mjutils/client/render/model/ModelWrapperDisplayList.java
@@ -0,0 +1,81 @@
+package io.github.mjaroslav.mjutils.client.render.model;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import lombok.val;
+import net.minecraftforge.client.model.IModelCustom;
+import net.minecraftforge.client.model.obj.WavefrontObject;
+import org.jetbrains.annotations.NotNull;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+
+import java.nio.IntBuffer;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.lwjgl.opengl.GL11.*;
+
+/**
+ * Thx Dahaka for GL Lists <a href="https://forum.mcmodding.ru/threads/uskorenie-rendera-modelej.10481/">guide</a>
+ *
+ * @author Dahaka
+ */
+@SideOnly(Side.CLIENT)
+public class ModelWrapperDisplayList implements IModelCustom {
+    private final Map<String, Integer> lists = new HashMap<>();
+    private final IntBuffer bufAll;
+    private final String type;
+
+    public ModelWrapperDisplayList(@NotNull WavefrontObject model) {
+        type = model.getType();
+        var list = glGenLists(model.groupObjects.size());
+        for (var obj : model.groupObjects) {
+            glNewList(list, GL11.GL_COMPILE);
+            model.renderPart(obj.name);
+            glEndList();
+            lists.put(obj.name, list++);
+        }
+        bufAll = initBuffer();
+    }
+
+    private @NotNull IntBuffer initBuffer() {
+        val buf = BufferUtils.createIntBuffer(lists.size());
+        for (var i : lists.values()) buf.put(i);
+        buf.flip();
+        return buf;
+    }
+
+    @Override
+    public @NotNull String getType() {
+        return type;
+    }
+
+    @Override
+    public void renderAll() {
+        glCallLists(bufAll);
+    }
+
+    @Override
+    public void renderOnly(@NotNull String... groupNames) {
+        if (groupNames == null || groupNames.length == 0) return;
+        for (String group : groupNames) renderPart(group);
+    }
+
+    @Override
+    public void renderPart(@NotNull String partName) {
+        val list = lists.get(partName);
+        if (list != null) glCallList(list);
+    }
+
+    @Override
+    public void renderAllExcept(@NotNull String... groupNames) {
+        if (groupNames == null || groupNames.length == 0) {
+            renderAll();
+            return;
+        }
+        for (var entry : lists.entrySet())
+            if (Arrays.binarySearch(groupNames, entry.getKey(), String::compareTo) < 0)
+                glCallList(entry.getValue());
+    }
+}
---
+++ src/main/java/io/github/mjaroslav/mjutils/client/render/model/obj/IconScaledFace.java
@@ -0,0 +1,90 @@
+package io.github.mjaroslav.mjutils.client.render.model.obj;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import lombok.val;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.Vec3;
+import net.minecraftforge.client.model.obj.TextureCoordinate;
+import net.minecraftforge.client.model.obj.Vertex;
+import org.jetbrains.annotations.NotNull;
+
+public class IconScaledFace {
+    public Vertex[] vertices;
+    public Vertex[] vertexNormals;
+    public Vertex faceNormal;
+    public TextureCoordinate[] textureCoordinates;
+
+    @SideOnly(Side.CLIENT)
+    public void addFaceForRender(@NotNull Tessellator tessellator) {
+        addFaceForRender(tessellator, 0.0005F);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void addFaceForRender(@NotNull Tessellator tessellator, float textureOffset) {
+        if (faceNormal == null) faceNormal = calculateFaceNormal();
+        tessellator.setNormal(faceNormal.x, faceNormal.y, faceNormal.z);
+        float averageU = 0F;
+        float averageV = 0F;
+        if ((textureCoordinates != null) && (textureCoordinates.length > 0)) {
+            for (var textureCoordinate : textureCoordinates) {
+                averageU += textureCoordinate.u;
+                averageV += textureCoordinate.v;
+            }
+            averageU = averageU / textureCoordinates.length;
+            averageV = averageV / textureCoordinates.length;
+        }
+        float offsetU, offsetV;
+        for (var i = 0; i < vertices.length; ++i)
+            if ((textureCoordinates != null) && (textureCoordinates.length > 0)) {
+                offsetU = textureOffset;
+                offsetV = textureOffset;
+                if (textureCoordinates[i].u > averageU) offsetU = -offsetU;
+                if (textureCoordinates[i].v > averageV) offsetV = -offsetV;
+                tessellator.addVertexWithUV(vertices[i].x, vertices[i].y, vertices[i].z, textureCoordinates[i].u
+                    + offsetU, textureCoordinates[i].v + offsetV);
+            } else tessellator.addVertex(vertices[i].x, vertices[i].y, vertices[i].z);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void addFaceForRender(@NotNull IIcon icon, @NotNull Tessellator tessellator) {
+        addFaceForRender(icon, tessellator, 0.0005F);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void addFaceForRender(@NotNull IIcon icon, @NotNull Tessellator tessellator, float textureOffset) {
+        if (faceNormal == null) faceNormal = calculateFaceNormal();
+        tessellator.setNormal(faceNormal.x, faceNormal.y, faceNormal.z);
+        float averageU = 0F;
+        float averageV = 0F;
+        if ((textureCoordinates != null) && (textureCoordinates.length > 0)) {
+            for (var textureCoordinate : textureCoordinates) {
+                averageU += icon.getInterpolatedU(16 * textureCoordinate.u);
+                averageV += icon.getInterpolatedV(16 * textureCoordinate.v);
+            }
+            averageU = averageU / textureCoordinates.length;
+            averageV = averageV / textureCoordinates.length;
+        }
+        float offsetU, offsetV;
+        for (var i = 0; i < vertices.length; ++i)
+            if ((textureCoordinates != null) && (textureCoordinates.length > 0)) {
+                offsetU = textureOffset;
+                offsetV = textureOffset;
+                if (icon.getInterpolatedU(16 * textureCoordinates[i].u) > averageU) offsetU = -offsetU;
+                if (icon.getInterpolatedV(16 * textureCoordinates[i].v) > averageV) offsetV = -offsetV;
+                tessellator.addVertexWithUV(vertices[i].x, vertices[i].y, vertices[i].z,
+                    icon.getInterpolatedU(16 * textureCoordinates[i].u) + offsetU,
+                    icon.getInterpolatedV(16 * textureCoordinates[i].v) + offsetV);
+            } else tessellator.addVertex(vertices[i].x, vertices[i].y, vertices[i].z);
+    }
+
+    public Vertex calculateFaceNormal() {
+        val v1 = Vec3.createVectorHelper(vertices[1].x - vertices[0].x, vertices[1].y - vertices[0].y,
+            vertices[1].z - vertices[0].z);
+        val v2 = Vec3.createVectorHelper(vertices[2].x - vertices[0].x, vertices[2].y - vertices[0].y,
+            vertices[2].z - vertices[0].z);
+        val normalVector = v1.crossProduct(v2).normalize();
+        return new Vertex((float) normalVector.xCoord, (float) normalVector.yCoord, (float) normalVector.zCoord);
+    }
+}
