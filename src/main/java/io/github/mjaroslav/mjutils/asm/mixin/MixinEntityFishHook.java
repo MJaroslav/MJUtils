f8ef2fc9950ea2d0a17005313582ce91ca2e67bc
Yaroslav Novitsky <rcr3211@gmail.com>
2022-11-15 19:50:16 +0300

    refactor: FishingSuccessEvent;

--- src/main/java/io/github/mjaroslav/mjutils/asm/mixin/MixinEntityFishHook.java
+++ src/main/java/io/github/mjaroslav/mjutils/asm/mixin/MixinEntityFishHook.java
@@ -1,6 +1,7 @@
 package io.github.mjaroslav.mjutils.asm.mixin;
 
-import io.github.mjaroslav.mjutils.object.game.event.FishingSuccessEvent;
+import io.github.mjaroslav.mjutils.event.FishingSuccessEvent;
+import lombok.val;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
@@ -11,6 +12,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.common.FishingHooks;
 import net.minecraftforge.common.MinecraftForge;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,27 +29,28 @@ public abstract class MixinEntityFishHook extends Entity {
 
     // Replace drop by event
     @Redirect(method = "func_146034_e",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/world/World;spawnEntityInWorld(Lnet/minecraft/entity/Entity;)Z",
-                    ordinal = 0))
+        at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/World;spawnEntityInWorld(Lnet/minecraft/entity/Entity;)Z",
+            ordinal = 0))
     private boolean injectedEntityItem(World world, Entity entity) {
-        if (fishingSuccessEvent != null && !fishingSuccessEvent.isCanceled() && fishingSuccessEvent.catchStack != null)
-            world.spawnEntityInWorld(new EntityItem(worldObj, posX, posY, posZ, fishingSuccessEvent.catchStack));
+        if (fishingSuccessEvent != null && !fishingSuccessEvent.isCanceled() && fishingSuccessEvent.caughtItem != null)
+            world.spawnEntityInWorld(new EntityItem(worldObj, posX, posY, posZ, fishingSuccessEvent.caughtItem));
         return false;
     }
 
     // Replacing XP by event and resetting of event
     @Redirect(method = "func_146034_e",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/world/World;spawnEntityInWorld(Lnet/minecraft/entity/Entity;)Z",
-                    ordinal = 1))
+        at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/World;spawnEntityInWorld(Lnet/minecraft/entity/Entity;)Z",
+            ordinal = 1))
     private boolean injectedEntityXPOrb(World world, Entity entity) {
         if (fishingSuccessEvent != null) {
-            if (!fishingSuccessEvent.isCanceled() && fishingSuccessEvent.exp > 0)
-                world.spawnEntityInWorld(new EntityXPOrb(worldObj, fishingSuccessEvent.fisher.posX,
-                        fishingSuccessEvent.fisher.posY, fishingSuccessEvent.fisher.posZ, fishingSuccessEvent.exp));
-            if (fishingSuccessEvent.incStat && fishingSuccessEvent.category != null)
-                fishingSuccessEvent.fisher.addStat(fishingSuccessEvent.category.stat, 1);
+            if (!fishingSuccessEvent.isCanceled() && fishingSuccessEvent.caughtExperience > 0)
+                world.spawnEntityInWorld(new EntityXPOrb(worldObj, fishingSuccessEvent.fishman.posX,
+                    fishingSuccessEvent.fishman.posY, fishingSuccessEvent.fishman.posZ,
+                    fishingSuccessEvent.caughtExperience));
+            if (fishingSuccessEvent.increaseStatistic && fishingSuccessEvent.caughtCategory != null)
+                fishingSuccessEvent.fishman.addStat(fishingSuccessEvent.caughtCategory.stat, 1);
         }
         fishingSuccessEvent = null;
         return false;
@@ -55,17 +58,15 @@ public abstract class MixinEntityFishHook extends Entity {
 
     // Add stat removing and call injected event
     @Inject(method = "func_146033_f",
-            at = @At("HEAD"), cancellable = true)
-    private void func_146033_f(CallbackInfoReturnable<ItemStack> ci) {
-        float chance = worldObj.rand.nextFloat();
-        int luck = EnchantmentHelper.func_151386_g(((EntityFishHook) (Object) this).field_146042_b);
-        int lure = EnchantmentHelper.func_151387_h(((EntityFishHook) (Object) this).field_146042_b);
-        FishingHooks.FishableCategory category = net.minecraftforge.common.FishingHooks.getFishableCategory(chance,
-                luck, lure);
-        ItemStack catchStack = FishingHooks.getRandomFishable(rand, chance, luck, lure);
+        at = @At("HEAD"), cancellable = true)
+    private void func_146033_f(@NotNull CallbackInfoReturnable<ItemStack> ci) {
+        val chance = worldObj.rand.nextFloat();
+        val luck = EnchantmentHelper.func_151386_g(((EntityFishHook) (Object) this).field_146042_b);
+        val lure = EnchantmentHelper.func_151387_h(((EntityFishHook) (Object) this).field_146042_b);
+        val category = net.minecraftforge.common.FishingHooks.getFishableCategory(chance, luck, lure);
+        val catchStack = FishingHooks.getRandomFishable(rand, chance, luck, lure);
         fishingSuccessEvent = new FishingSuccessEvent(((EntityFishHook) (Object) this).field_146042_b,
-                (EntityFishHook) (Object) this, category, catchStack,
-                chance, luck, lure, rand.nextInt(6) + 1);
+            (EntityFishHook) (Object) this, category, catchStack, rand.nextInt(6) + 1, chance, luck, lure, true);
         MinecraftForge.EVENT_BUS.post(fishingSuccessEvent);
         ci.setReturnValue(new ItemStack(Blocks.air));
     }
--- src/main/java/io/github/mjaroslav/mjutils/asm/mixin/MixinFishingHooks.java
+++ src/main/java/io/github/mjaroslav/mjutils/asm/mixin/MixinFishingHooks.java
@@ -6,6 +6,7 @@ import net.minecraft.util.WeightedRandom;
 import net.minecraft.util.WeightedRandom.Item;
 import net.minecraft.util.WeightedRandomFishable;
 import net.minecraftforge.common.FishingHooks;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -18,11 +19,10 @@ public abstract class MixinFishingHooks {
     // Empty list exception dodging
     @SuppressWarnings("rawtypes")
     @Redirect(method = "getRandomFishable(Ljava/util/Random;FII)Lnet/minecraft/item/ItemStack;",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/WeightedRandom;getRandomItem(Ljava/util/Random;" +
-                    "Ljava/util/Collection;)Lnet/minecraft/util/WeightedRandom$Item;"))
-    private static Item injectedGetRandomFishable(Random rand, Collection list) {
-        if (list.isEmpty())
-            return new WeightedRandomFishable(new ItemStack(Blocks.air), 0);
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/util/WeightedRandom;getRandomItem(Ljava/util/Random;" +
+            "Ljava/util/Collection;)Lnet/minecraft/util/WeightedRandom$Item;"))
+    private static Item injectedGetRandomFishable(Random rand, @NotNull Collection list) {
+        if (list.isEmpty()) return new WeightedRandomFishable(new ItemStack(Blocks.air), 0);
         else return WeightedRandom.getRandomItem(rand, list);
     }
 }
